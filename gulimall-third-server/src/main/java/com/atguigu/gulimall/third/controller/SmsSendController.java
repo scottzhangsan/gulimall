2d29459724eb15329b367fe421ceadf5bc2e95f4
pkum5y <Yazhou_Zhang.sgmext@SAIC-GM.com>
2020-05-06 13:05:11 +0800

    添加发送短信验证码的功能

---
+++ gulimall-third-server/src/main/java/com/atguigu/gulimall/third/controller/SmsSendController.java
@@ -0,0 +1,30 @@
+package com.atguigu.gulimall.third.controller;
+
+import com.atguigu.common.utils.R;
+import com.atguigu.gulimall.third.component.SmsComponent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sms")
+public class SmsSendController {
+    @Autowired
+    private SmsComponent smsComponent ;
+
+    /**
+     * 发送验证码
+     * @param phone
+     * @param code
+     * @return
+     */
+    @GetMapping("/send")
+    public R sendSms(String phone, String code){
+        smsComponent.sendSmsCode(phone,code);
+        return R.ok() ;
+    }
+
+
+
+}
