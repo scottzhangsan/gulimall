60ca065f4013564f85727cbb89af10fa0c6a28d9
pkum5y <Yazhou_Zhang.sgmext@SAIC-GM.com>
2020-06-07 12:00:29 +0800

    订单整合springsession

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -4,10 +4,12 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableFeignClients(basePackages = "com.atguigu.gulimall.order.feign")
 @SpringBootApplication
 @EnableRabbit
+@EnableRedisHttpSession
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
---
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/config/GulimallSessionConfig.java
@@ -0,0 +1,25 @@
+package com.atguigu.gulimall.order.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
+@Configuration
+public class GulimallSessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer(){
+        DefaultCookieSerializer cookieSerializer = new DefaultCookieSerializer() ;
+        cookieSerializer.setCookieName("GULIMALL_SESSION"); // session name
+        cookieSerializer.setDomainName("gulimal.com");  //cookie 作用域
+        return cookieSerializer ;
+    }
+
+    @Bean
+    public RedisSerializer createRedisSerializer(){
+       return new  GenericJackson2JsonRedisSerializer() ;   //设置redis序列化
+    }
+}
